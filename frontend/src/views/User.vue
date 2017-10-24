6ac1c9d0decf21c52d18fe9bf70320ef14ed1df3
ParkSeokje <molla4455@gmail.com>
2017-10-24 18:42:21 +0900

    사용자 등록 개발중..

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -1,25 +1,70 @@
 <template>
   <div class="animated fadeIn">
-    <b-card>
-      <b-table small striped bordered hover :items="items" :fields="fields"></b-table>
-    </b-card>
-    <!--<span v-for="item in items">{{item.first_name}}</span>-->
+    <div class="row">
+      <div class="col-12">
+        <b-card header="사용자 목록">
+          <b-button href="#" variant="primary">사용자 등록</b-button>
+          <br><br>
+
+          <div class="row">
+            <div class="col-md-3">
+              <b-form-fieldset horizontal label="페이지당 행개수" :label-cols="6">
+                <b-form-select :options="pageOptions" v-model="perPage" />
+              </b-form-fieldset>
+            </div>
+            <div class="col-md-3">
+              <b-form-fieldset horizontal label="필터" :label-cols="3">
+                <b-form-input v-model="filter" placeholder="검색어 입력" />
+              </b-form-fieldset>
+            </div>
+            <div class="col-md-6">
+              <b-pagination align="right" :total-rows="totalRows" :per-page="perPage" v-model="currentPage" />
+            </div>
+          </div>
+
+          <b-table striped
+            :items="items"
+            :fields="fields">
+            <template slot="actions" scope="row">
+              <b-btn variant="primary" size="sm" @click.stop="details(row.item,row.index,$event.target)">보기</b-btn>
+              <b-btn variant="danger" size="sm" @click.stop="remove(row.item,row.index,$event.target)">삭제</b-btn>
+            </template>
+          </b-table>
+        </b-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const fields = {
+  user_type: { label: '구분', sortable: true },
+  name: { label: '이름', sortable: true },
+  cell_no: { label: '핸드폰', sortable: false },
+  actions: { label: 'Actions' }
+}
+
+const items = [
+  { isActive: true, user_type: 'SV', name: 'Dickerson', cell_no: '010-2009-1407', memo: 'blah blah' },
+  { isActive: true, user_type: 'SV', name: 'Dickerson', cell_no: '010-2009-1407', memo: 'blah blah' },
+  { isActive: true, user_type: 'SV', name: 'Dickerson', cell_no: '010-2009-1407', memo: 'blah blah' }
+]
+
 export default {
   name: 'user',
   data () {
     return {
-      // Note 'isActive' is left out and will not appear in the rendered table
-      fields: [ 'first_name', 'last_name', 'age' ],
-      items: [
-        { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-      ]
+      fields: fields,
+      items: items,
+      currentPage: 1,
+      perPage: 5,
+      totalRows: items.length,
+      pageOptions: [
+        { text: 5, value: 5 },
+        { text: 10, value: 10 },
+        { text: 15, value: 15 }
+      ],
+      filter: null
     }
   }
 }
