ba7b78681b9b0ec43e11b6f23314006d522bf5a2
ParkSeokje <molla4455@gmail.com>
2017-10-24 12:28:51 +0900

    pm2 deploy 테스트중..

---
+++ ecosyste.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    name: 'orange-checklist',
+    script: './bin/www',
+    env: {
+      "NODE_ENV": "development"
+    },
+    env_production: {
+      "NODE_ENV": "production"
+    }
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-78-245-22.ap-northeast-2.compute.amazonaws.com',
+      key: '~/.ssh/orangemanu.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:Parkseokje/orange-checklist.git',
+      path: '/home/ubuntu/orange-checklist',
+      'post-deploy': 'cd ./backend && npm install && pm2 startOrRestart ../ecosystem.config.js'
+    }
+  }
+}
