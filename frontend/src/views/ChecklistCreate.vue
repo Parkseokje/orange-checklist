02c76b9f78647f6b0334c1ae632ce3f2dcd2adaf
ParkSeokje <molla4455@gmail.com>
2017-11-09 11:11:52 +0900

    pm2 환경설정 테스트중..

--- frontend/src/views/ChecklistCreate.vue
+++ frontend/src/views/ChecklistCreate.vue
@@ -7,21 +7,21 @@
           <b-row>
             <b-col lg="2">
               <b-form-group label="체크리스트 구분">
-                <b-form-select v-model="checklistTypeSelected" :options="checkListTypes" @change="onChecklistTypeChange"></b-form-select>
+                <b-form-select v-model="form.list_type" :options="checkListTypes" @change="onChecklistTypeChange"></b-form-select>
               </b-form-group>
             </b-col>
             <b-col>
               <b-form-group label="체크리스트명">
-                <b-form-input placeholder="텍스트를 입력하세요"></b-form-input>
+                <b-form-input placeholder="텍스트를 입력하세요" v-model="form.title"></b-form-input>
               </b-form-group>
             </b-col>
           </b-row>
           <b-form-group label="평가항목 배점">
-            <input-tag placeholder="추가" :tags="sampleAnswers"></input-tag>
+            <input-tag placeholder="입력+ENTER" :tags="form.scoring"></input-tag>
             <b-btn variant="link" size="sm" @click="reversSampleAnswers">순서변경</b-btn>
           </b-form-group>
           <b-form-group label="비고">
-            <quill-editor :options="editorOption"></quill-editor>
+            <quill-editor ref="memo" :options="editorOption" :content="form.memo" @change="onEditorChange($event)"></quill-editor>
           </b-form-group>
         </b-form>
 
@@ -33,28 +33,41 @@
                 <b-tab title="평가항목 추가" active>
                   <b-form>
                     <b-form-group label="대분류" horizontal>
-                      <v-select label="name" placeholder="선택하세요"></v-select>
+                      <v-select label="name" placeholder="선택하세요"
+                        v-model="dropdown1Selected"
+                        :options="dropDown1Items"
+                        :reset-on-options-change="true"
+                      ></v-select>
                     </b-form-group>
                     <b-form-group label="중분류" horizontal>
-                      <v-select label="name" placeholder="선택하세요"></v-select>
+                      <v-select label="name" placeholder="선택하세요"
+                        v-model="dropdown2Selected"
+                        :options="dropDown2Items"
+                        :reset-on-options-change="true"
+                      ></v-select>
                     </b-form-group>
                     <b-form-group label="소분류" horizontal>
-                      <v-select label="name" placeholder="선택하세요"></v-select>
+                      <v-select label="name" placeholder="선택하세요"
+                        v-model="dropdown3Selected"
+                        :options="dropDown3Items"
+                        :reset-on-options-change="true"
+                      ></v-select>
                     </b-form-group>
                     <b-form-group label="평가항목명">
-                      <b-form-input type="text" required placeholder="텍스트를 입력하세요" v-model="form.question">
+                      <b-form-input type="text" required placeholder="텍스트를 입력하세요" v-model="item1Title">
                       </b-form-input>
                     </b-form-group>
                     <b-form-group label="기재사항">
-                      <quill-editor :options="editorOption"></quill-editor>
+                      <quill-editor :options="editorOption" :content="item1Input" @change="onEditorChange($event)"></quill-editor>
                     </b-form-group>
                     <b-form-group label="유의사항">
-                      <quill-editor :options="editorOption"></quill-editor>
+                      <quill-editor :options="editorOption" :content="item1Precaution" @change="onEditorChange($event)"></quill-editor>
                     </b-form-group>
                     <div class="d-flex justify-content-between">
-                      <b-form-checkbox id="checkbox1"
-                        value="accepted"
-                        unchecked-value="not_accepted">
+                      <b-form-checkbox
+                        v-model="item1FileAttach"
+                        value="1"
+                        unchecked-value="0">
                         파일 첨부 가능
                       </b-form-checkbox>
                       <b-btn variant="primary">추가</b-btn>
@@ -63,16 +76,17 @@
                 </b-tab>
                 <b-tab title="입력항목 추가">
                   <b-form-group label="입력항목명">
-                    <b-form-input type="text" required placeholder="텍스트를 입력하세요" v-model="form.question">
+                    <b-form-input type="text" required placeholder="텍스트를 입력하세요" v-model="item2Title">
                     </b-form-input>
                   </b-form-group>
                   <b-form-group label="유의사항">
-                    <quill-editor :options="editorOption"></quill-editor>
+                    <quill-editor :options="editorOption" :content="item2Precaution" @change="onEditorChange($event)"></quill-editor>
                   </b-form-group>
                   <div class="d-flex justify-content-between">
-                    <b-form-checkbox id="checkbox1"
-                      value="accepted"
-                      unchecked-value="not_accepted">
+                    <b-form-checkbox
+                      v-model="item2FileAttach"
+                      value="1"
+                      unchecked-value="0">
                       파일 첨부 가능
                     </b-form-checkbox>
                     <b-btn mr-auto variant="primary">추가</b-btn>
@@ -94,14 +108,14 @@
             </draggable>
             </b-card>
           </b-col>
-        </b-row>        
+        </b-row>
       </b-card>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import { quillEditor } from 'vue-quill-editor'
 import draggable from 'vuedraggable'
 
@@ -115,41 +129,63 @@ export default {
 
   data () {
     return {
-      checklistTypeSelected: null,
       checklistQuizTypeSelected: null,
+      dropdown1Selected: null,
+      dropdown2Selected: null,
+      dropdown3Selected: null,
       checklistQuizTypes: [
         { text: '선택형', value: 'S' },
         { text: '단답형', value: 'T' },
         { text: '다답형', value: 'M' }
       ],
-      form: {
-        question: null
-      },
       sampleAnswers: [],
       itemList: [
         { id: 1, name: 'Macdonald1' },
         { id: 2, name: 'Macdonald2' },
         { id: 3, name: 'Macdonald3' },
         { id: 4, name: 'Macdonald4' }
-      ]
+      ],
+      form: {
+        title: null,
+        list_type: null,
+        scoring: null,
+        memo: null,
+        items: []
+      },
+      item1Title: null,
+      item1Input: null,
+      item1Precaution: null,
+      item1FileAttach: 0,
+      item2Title: null,
+      item2Precaution: null,
+      item2FileAttach: 0
     }
   },
 
   methods: {
+    ...mapActions([
+      'fetchCategoryLists'
+    ]),
+
     reversSampleAnswers () {
-      this.sampleAnswers.reverse()
+      this.form.scoring.reverse()
+    },
+
+    onEditorChange ({ editor, html, text }) {
+      console.log('editor change!', editor, html, text)
+      this.form.memo = html
     },
 
     onChecklistTypeChange (selected) {
       switch (selected) {
         case 'supervisor':
-          this.sampleAnswers = [ '1', '0' ]
+          this.form.scoring = [ '1', '0' ]
           break
         case 'shopper':
-          this.sampleAnswers = [ '7', '6', '5', '4', '3', '2', '1' ]
+          this.form.scoring = [ '7', '6', '5', '4', '3', '2', '1' ]
           break
         case 'agent':
-          this.sampleAnswers = [ '5', '10' ]
+          this.form.scoring = [ '5', '10' ]
           break
         default:
           break
@@ -159,9 +195,36 @@ export default {
 
   computed: {
     ...mapGetters({
+      bigItems: 'getBigCategories',
+      middleItems: 'getMiddleCategories',
+      smallItems: 'getSmallCategories',
       checkListTypes: 'checkListTypes',
       editorOption: 'editorOption'
-    })
+    }),
+
+    dropDown1Items () {
+      return this.bigItems
+    },
+
+    dropDown2Items () {
+      if (this.dropdown1Selected) {
+        return this.middleItems(this.dropdown1Selected.id)
+      } else {
+        return []
+      }
+    },
+
+    dropDown3Items () {
+      if (this.dropdown2Selected) {
+        return this.smallItems(this.dropdown2Selected.id)
+      } else {
+        return []
+      }
+    }
+  },
+
+  created () {
+    this.fetchCategoryLists()
   }
 }
 </script>
