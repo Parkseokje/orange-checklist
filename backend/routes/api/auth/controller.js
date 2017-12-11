3c85bf658b31db5ad7346830b7e14a6c989cca59
ParkSeokje <molla4455@gmail.com>
2017-12-11 14:24:23 +0900

    암호 재설정 페이지 작업중

--- backend/routes/api/auth/controller.js
+++ backend/routes/api/auth/controller.js
@@ -120,7 +120,7 @@ exports.signin = (req, res) => {
 }
 
 exports.forgotPassword = (req, res) => {
-  console.log(config.mailer)
+  console.log(config.mailer.email, config.mailer.password)
   pool.getConnection((err, connection) => {
     if (err) {
       console.log(error)
